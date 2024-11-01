3a68fd38882a93270f9d32be523baa19ecd68aa5
sniperkla <sniperkla@gmail.com>
2024-11-01 21:54:04 +0700

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 const { OpenAI } = require('openai')
+const axios = require('axios')
 
 const fs = require('fs')
 app.use(express.json())
@@ -12,25 +13,39 @@ const key = process.env.API_KEY
 const openai = new OpenAI({
   apiKey: key
 })
-
+const mp3 = fs.createReadStream('./path/www.mp3')
 app.post('/api/voice', async (req, res) => {
-  try {
-    console.log('fucking key', key)
-    const mp3 = fs.createReadStream('./path/www.mp3')
-    const response = await openai.audio.translations.create({
-      file: mp3,
-      model: 'whisper-1',
-      response_format: 'text'
-    })
-    if (response) {
-      console.log(response.text)
-    } // Access the transcription text
-  } catch (error) {
-    console.error(
-      'Error during transcription:',
-      error.response ? error.response.data : error.message
-    )
-  }
+  const response = await axios.post(
+    'https://api.openai.com/v1/audio/transcriptions',
+    {
+      file: mp3, // Access the actual file data
+      model: 'whisper-1'
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${key}`
+      }
+    }
+  )
+
+  console.log('response', response.data)
+  //   try {
+  //     console.log('fucking key', key)
+  //     const mp3 = fs.createReadStream('./path/www.mp3')
+  //     const response = await openai.audio.translations.create({
+  //       file: mp3,
+  //       model: 'whisper-1',
+  //       response_format: 'text'
+  //     })
+  //     if (response) {
+  //       console.log(response.text)
+  //     } // Access the transcription text
+  //   } catch (error) {
+  //     console.error(
+  //       'Error during transcription:',
+  //       error.response ? error.response.data : error.message
+  //     )
+  //   }
   res.status(200).json({ message: 'Success' })
 })
 
